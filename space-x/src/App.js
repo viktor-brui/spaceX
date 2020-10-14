9306fd07807ffd8ec02a833cd39383bdd26eec9e
viktor <viktor-brui@mail.ru>
2020-10-14 16:55:59 +0300

    feat: add content lending main page

--- space-x/src/App.js
+++ space-x/src/App.js
@@ -1,52 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Features from './components/Features/Features';
+import Calendar from './components/Calendar/Calendar';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import './style.css';
+import Details from './components/Details/Details';
 
 
 function App() {
   return (
     <>
-      <header className="header">
-          <img
-              src={logo}
-              alt="Logo Space X"
-              className="logo"
-          />
-          <nav className="main-nav nav">
-            <ul className="list">
-              <li className="item">
-                <a href="/" className="item-link">Falcon 1</a>
-              </li>
-              <li className="item">
-                <a href="/" className="item-link">Falcon 9</a>
-              </li>
-              <li className="item">
-                <a href="/" className="item-link">Falcon Heavy</a>
-              </li>
-              <li className="item">
-                <a href="/" className="item-link">Updates</a>
-              </li>
-            </ul>
-          </nav>
-          <nav className="secondary-nav">
-            <ul className="list">
-              <li className="item">
-                <a href="/" className="item-link">Home</a>
-              </li>
-              <li className="item">
-                <a href="calendar.html" className="item-link">Calendar</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <section className="main">
-          <h1 className="title">
-            Falcon 1
-          </h1>
-          <div className="video-container">
-            <video className="video" autoPlay loop muted src="./video/moon.mp4"></video>
-          </div>
-        </section>
+      <Header />
+      <Main />
+      <Features />
+      {/* <Calendar /> */}
+      {/* <Details /> */}
+      <Footer />
     </>
     
 
